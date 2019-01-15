72c8c664940568274671182019fa3c98e277097a
Masoud MP <mmp.subscribe@gmail.com>
2019-01-14 18:13:51 -0800

    Enable DELETE in cors

--- startup/routes.js
+++ startup/routes.js
@@ -11,9 +11,10 @@ const returns = require("../routes/returns");
 module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     next();
   });
