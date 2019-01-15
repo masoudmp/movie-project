38b671c9cb32545ef5cb6f2d0a2a56c92c75492e
Masoud MP <mmp.subscribe@gmail.com>
2019-01-14 16:23:20 -0800

    enabling cors

--- startup/routes.js
+++ startup/routes.js
@@ -9,6 +9,14 @@ const auth = require("../routes/auth");
 const returns = require("../routes/returns");
 
 module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use(express.json());
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
